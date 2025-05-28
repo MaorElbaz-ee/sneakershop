23aebf45c46a6390c816517dd5f2d4abf82c1f4f
Maor Elbaz <maorelbaz90@gmail.com>
2025-05-28 22:17:29 +0300

    Update customers.h

--- customers.h
+++ customers.h
@@ -1,8 +1,4 @@
-//////////////////////////////////////////
-// Created by: Nikolai Akulov, 316868132//
-//             Maor Elbaz,     208418293//
-//             Shay Shitrit,   207895798//
-//////////////////////////////////////////
+
 #pragma once
 #include "items.h"
 
